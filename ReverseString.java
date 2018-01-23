4fc207fa2f47b1c4fb2345d1bd0d5a359605a80f
Chris Maltais <chris.maltais@hotmail.ca>
2018-01-23 12:11:51 -0500

    Explaining purpose of StringBuilder()

--- ReverseString.java
+++ ReverseString.java
@@ -1,10 +1,13 @@
 // Write a function that takes a string as input and returns the string reversed.
+// StringBuilder creates a reziable array of all the strings (think of a sentence), 
+// copying them back only when necessary.
 
 public class ReverseString {
 	public String reverseString(String s) {
     		if (s == null){
         	return null;
     		}
-   		return new StringBuilder(s).reverse().toString();
+		// StringBuilder(s) turns the string into a resizable array of characters
+   		return new StringBuilder(s).reverse().toString(); 
 	}
 }
