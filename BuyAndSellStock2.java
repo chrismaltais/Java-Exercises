daecde863db933b064add6e78a8d15cb1678acbb
Chris Maltais <chris.maltais@hotmail.ca>
2018-08-15 23:01:51 -0400

    Add Buy and Sell Stock 2 (Infinite buy and sells)

---
+++ BuyAndSellStock2.java
@@ -0,0 +1,14 @@
+// Unlimited Buy and Sell, however cannot overlap.
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null) return 0;
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i-1]) {
+                profit += prices[i] - prices[i-1];
+            }
+        }
+        return profit;
+    }
+}
