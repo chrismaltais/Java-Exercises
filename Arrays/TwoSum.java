8630749bb18e6ac6fe55b6f580eb3a74ac77b648
chrismaltais <chris.maltais@hotmail.ca>
2018-08-29 10:46:10 -0400

    Add Two Sum

---
+++ Arrays/TwoSum.java
@@ -0,0 +1,20 @@
+// Given an array of integers,
+// return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, 
+// and you may not use the same element twice.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                result[0] = i;
+                result[1] = map.get(target - nums[i]);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
+    }
+}
